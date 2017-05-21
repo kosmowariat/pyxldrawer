70b75e47414455d1be9bdd9a471beaead24bc83a
sztal <stalaga@protonmail.com>
2017-05-21 15:45:09 +0200

    Add LineElement class in the complex module

--- pyxldrawer/elements/complex.py
+++ pyxldrawer/elements/complex.py
@@ -85,3 +85,88 @@ class TreeElement(object):
         """
         self.parent.draw(x, y, ws, wb)
         self.children.draw(x, y + self.parent.height, ws, wb)
+
+
+class LineElement(object):
+    """Horizontal or vertical line of elements
+    
+    Attributes:
+        height (int): total height of element / height of the heighes element
+        width (int): width of the widest element / total width of the elements
+        vertical (bool): is the line vertical or horizontal
+        elements (list): list of objects equiped with the 'draw' method
+    """
+    
+    ###########################################################################
+    
+    @property
+    def height(self):
+        return self._height
+    
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int) or value < 1:
+            raise TypeError('height has to be a positive integer.')
+        self._height = value
+    
+    @property
+    def width(self):
+        return self._width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int) or value < 1:
+            raise TypeError('width has to be a positive integer.')
+        self._width = value
+    
+    @property
+    def vertical(self):
+        return self._vertical
+    
+    @vertical.setter
+    def vertical(self, value):
+        if not isinstance(value, bool):
+            raise TypeError('vertical has to be a bool.')
+        self._vertical = value
+    
+    @property
+    def elements(self):
+        return self._elements
+    
+    @elements.setter
+    def elements(self, value):
+        if not isinstance(value, list):
+            raise TypeError('elements has to be a list.')
+        self._elements = value
+    
+    ###########################################################################
+    
+    def __init__(self, elements, vertical = False):
+        """Constructor method
+        """
+        self.elements = elements
+        self.vertical = vertical
+        if vertical:
+            self.height = sum([ x.height for x in elements ])
+            self.width = max([ x.width for x in elements ])
+        else:
+            self.height = max([ x.height for x in elements ])
+            self.width = sum([ x.width for x in elements ])
+    
+    def draw(self, x, y, ws, wb):
+        """Draw LineElements in a worksheet
+        
+        Args:
+            x (int): x-coordinate
+            y (int): y-coordinate
+            ws (xlsxwriter.worksheet.Worksheet): worksheet to draw in
+            wb (xlsxwriter.workbook.Workbook): workbook the worksheet is in
+        """
+        if self.vertical:
+            for elem in self.elements:
+                elem.draw(x, y, ws, wb)
+                x += elem.height
+        else:
+            for elem in self.elements:
+                elem.draw(x, y, ws, wb)
+                y += elem.width
